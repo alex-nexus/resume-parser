52f7fb4f424e97f285d04db94c46a811a0993567
alex <alkchen@gmail.com>
2019-02-22 00:29:15 -0800

    clean up text

--- resume_parser/parse.py
+++ resume_parser/parse.py
@@ -1,14 +1,24 @@
-import multiprocessing as mp
-
-from resume_parser.convert import convert_to_text
 from resume_parser.extractor import Extractor
+from resume_parser.convert import convert_to_text
+
+import multiprocessing as mp
+import nltk
+from nltk.corpus import stopwords
+
+stop_words = stopwords.words('english')
+
 
 PROCESSES = 4
 
 
 def parse_batch(resume_files, keywords):
   texts = [convert_to_text(resume_file) for resume_file in resume_files]
+  texts = [clean_text(text) for text in texts]
   pool = mp.Pool(processes=PROCESSES)
   resumes = pool.map(Extractor().extract, texts)
   print(resumes)
   return resumes
+
+
+def clean_text(text):
+  return ' '.join([i for i in text.split() if i not in stop_words])
